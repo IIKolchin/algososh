39805f44aa18aa7a4ba4782a4d9dfc97f43be95d
Apple <IIKolchin@yandex.ru>
2022-07-14 22:46:30 +0300

    add fibonacci

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,69 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, { ChangeEvent, SetStateAction, useState } from 'react';
+import styles from './fibonacci-page.module.css'
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { Input } from '../ui/input/input';
+import { Button } from '../ui/button/button';
+import { Circle } from '../ui/circle/circle';
+import { delay } from '../../utils/utils';
+import { SHORT_DELAY_IN_MS } from '../../constants/delays';
+import { getFibonacciNumbers } from './utils';
 
 export const FibonacciPage: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [numberArr, setNumberArr] = useState<number[]>([]);
+  const [isLoader, setIsLoader] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const fibonacciRender = async (n: number) => {
+   const arrFib = getFibonacciNumbers(n)
+   const arrRender:any = [];
+   while (arrFib.length !== 0) {
+    setIsLoader(true);
+    setIsDisabled(true)
+    await delay(SHORT_DELAY_IN_MS)
+    arrRender.push(arrFib.shift())
+    setNumberArr([...arrRender])
+   }
+   setIsLoader(false);
+   setIsDisabled(false)
+  }
+
   return (
-    <SolutionLayout title="Последовательность Фибоначчи">
-     
+    <SolutionLayout title='Последовательность Фибоначчи'>
+      <div className={styles.container}>
+        <div className={styles.input}>
+          <Input
+            max={19}
+            onChange={onChange}
+            value={input}
+            name={'text'}
+            type = "number"
+            isLimitText
+            disabled={isDisabled}
+          />
+        </div>
+        <Button
+          text='Раcсчитать'
+          onClick={() => fibonacciRender(Number(input))}
+          isLoader={isLoader}
+        />
+      </div>
+      <ul className={styles.str}>
+        { numberArr.map((el, index) => {
+          return (
+            <li key={index} className={styles.circle}>
+              <Circle
+                letter={String(el)}
+                index={index}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,4 +1,9 @@
-import React, { ChangeEvent, FormEventHandler, SetStateAction, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FormEventHandler,
+  SetStateAction,
+  useState,
+} from 'react';
 import styles from './string.module.css';
 import { Button } from '../ui/button/button';
 import { Input } from '../ui/input/input';
@@ -10,59 +15,49 @@ import { DELAY_IN_MS } from '../../constants/delays';
 
 export const StringComponent: React.FC = () => {
   const [input, setInput] = useState('');
-  const [wordArr, setWordArr] = useState<string[]>([])
-  const [firstIndex, setFirstIndex] = useState<number>()
-  const [secondIndex, setSecondIndex] = useState<number>()
-  const [isLoader, setIsLoader] = useState(false)
-  // const [circleState, setCircleState] = useState(ElementStates.Default)
-  let circleState: ElementStates;
-  // console.log(circleState);
+  const [wordArr, setWordArr] = useState<string[]>([]);
+  const [firstIndex, setFirstIndex] = useState<number>();
+  const [secondIndex, setSecondIndex] = useState<number>();
+  const [isLoader, setIsLoader] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
   const reverseWord = async (str: string) => {
-const wordArray = str.split('');
-    setWordArr(wordArray)
-    
-   
- 
+    const wordArray = str.split('');
+    setWordArr(wordArray);
+
     let start = 0;
     let end = wordArray.length - 1;
     while (start <= end) {
-      setIsLoader(true)
-      setFirstIndex(start)
-      setSecondIndex(end)
-      await delay(DELAY_IN_MS)
-
-      swap(wordArray, start, end)
-      console.log(start)
-      console.log(end)
-      setWordArr([...wordArray])
-      await delay(DELAY_IN_MS)  
+      setIsLoader(true);
+      setIsDisabled(true)
+      setFirstIndex(start);
+      setSecondIndex(end);
+      await delay(DELAY_IN_MS);
+      swap(wordArray, start, end);
+      setWordArr([...wordArray]);
+      await delay(DELAY_IN_MS);
       start++;
       end--;
-
-      console.log(start === end - 1)
     }
-    setFirstIndex(100)
-    setSecondIndex(100)
-    setIsLoader(false)
-  }
-  
+    setFirstIndex(100);
+    setSecondIndex(100);
+    setIsLoader(false);
+    setIsDisabled(false)
+  };
 
-const changeColor = (start: number, end: number, index: number) => {
-  if(start === index || end === index) {
-return ElementStates.Changing
-  } else if (start >= index || end <= index) {
-    return ElementStates.Modified
-  } else if ( start === 100 || end === 100) {
-    return ElementStates.Modified
-  }
-}
-
-
+  const changeColor = (start: number, end: number, index: number) => {
+    if (start === index || end === index) {
+      return ElementStates.Changing;
+    } else if (start >= index || end <= index) {
+      return ElementStates.Modified;
+    } else if (start === 100 || end === 100) {
+      return ElementStates.Modified;
+    }
+  };
 
   return (
     <SolutionLayout title='Строка'>
@@ -73,21 +68,27 @@ return ElementStates.Changing
             onChange={onChange}
             value={input}
             name={'text'}
+            isLimitText
           />
-          <p className={styles.p}>Максимум — 11 символов</p>
         </div>
-        <Button text='Развернуть' onClick={() => reverseWord(input)} isLoader={isLoader}/>
+        <Button
+          text='Развернуть'
+          onClick={() => reverseWord(input)}
+          isLoader={isLoader}
+        />
       </div>
       <ul className={styles.str}>
-      {wordArr.map((el, index) => {
-        return( 
-        <li key={index} className={styles.circle}>
-        <Circle state={changeColor(firstIndex!, secondIndex!, index)} letter={el}/>
-        </li>
-        )
-      })}
+        {wordArr.map((el, index) => {
+          return (
+            <li key={index} className={styles.circle}>
+              <Circle
+                state={changeColor(firstIndex!, secondIndex!, index)}
+                letter={el}
+              />
+            </li>
+          );
+        })}
       </ul>
     </SolutionLayout>
   );
 };
-
