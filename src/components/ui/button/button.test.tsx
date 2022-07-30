c2dd344de53a11a8dae0c4168ff6f40098e26656
Apple <IIKolchin@yandex.ru>
2022-07-30 23:06:39 +0300

    finished jest test button and add jest tests circle

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,24 +1,33 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Button } from './button';
+import userEvent from '@testing-library/user-event';
+
+const onClick = jest.fn();
 
 describe('Button component', () => {
-    it('Button with text', () => {
-        const button = render(< Button text='Добавить' />)
-        expect(button).toMatchSnapshot();
-    })
+  it('Button with text', () => {
+    const button = render(<Button text='Добавить' />);
+    expect(button).toMatchSnapshot();
+  });
 
-    it('Button without text', () => {
-        const button = render(< Button />)
-        expect(button).toMatchSnapshot();
-    })
+  it('Button without text', () => {
+    const button = render(<Button />);
+    expect(button).toMatchSnapshot();
+  });
 
-    it('Button disabled', () => {
-        const button = render(< Button disabled/>)
-        expect(button).toMatchSnapshot();
-    })
+  it('Button disabled', () => {
+    const button = render(<Button disabled />);
+    expect(button).toMatchSnapshot();
+  });
 
-    it('Button isLoader', () => {
-        const button = render(< Button isLoader/>)
-        expect(button).toMatchSnapshot();
-    })
-})
+  it('Button isLoader', () => {
+    const button = render(<Button isLoader />);
+    expect(button).toMatchSnapshot();
+  });
+
+  it('Button onClick', () => {
+    render(<Button text='Добавить' onClick={onClick} />);
+    userEvent.click(screen.getByText('Добавить'));
+    expect(onClick).toHaveBeenCalled();
+  });
+});
