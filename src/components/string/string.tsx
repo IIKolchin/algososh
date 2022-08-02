64aef0d515fd70fe850bd473c21987f2d4c48f27
Apple <IIKolchin@yandex.ru>
2022-08-03 00:27:38 +0300

    add jest tests for string

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,15 +1,10 @@
-import React, {
-  ChangeEvent,
-  useState,
-} from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import styles from './string.module.css';
 import { Button } from '../ui/button/button';
 import { Input } from '../ui/input/input';
 import { SolutionLayout } from '../ui/solution-layout/solution-layout';
 import { Circle } from '../ui/circle/circle';
-import { delay, swap } from '../../utils/utils';
-import { DELAY_IN_MS } from '../../constants/delays';
-import { changeColor } from './utils';
+import { changeColor, reverseString } from './utils';
 
 export const StringComponent: React.FC = () => {
   const [input, setInput] = useState('');
@@ -21,30 +16,6 @@ export const StringComponent: React.FC = () => {
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
-  };
-
-  const reverseWord = async (str: string) => {
-    const wordArray = str.split('');
-    setWordArr(wordArray);
-
-    let start = 0;
-    let end = wordArray.length - 1;
-    while (start <= end) {
-      setIsLoader(true);
-      setIsDisabled(true);
-      setFirstIndex(start);
-      setSecondIndex(end);
-      await delay(DELAY_IN_MS);
-      swap(wordArray, start, end);
-      setWordArr([...wordArray]);
-      await delay(DELAY_IN_MS);
-      start++;
-      end--;
-    }
-    setFirstIndex(100);
-    setSecondIndex(100);
-    setIsLoader(false);
-    setIsDisabled(false);
   };
 
   return (
@@ -61,7 +32,16 @@ export const StringComponent: React.FC = () => {
         />
         <Button
           text='Развернуть'
-          onClick={() => reverseWord(input)}
+          onClick={() =>
+            reverseString(
+              input,
+              setWordArr,
+              setIsLoader,
+              setIsDisabled,
+              setFirstIndex,
+              setSecondIndex
+            )
+          }
           isLoader={isLoader}
         />
       </div>
--- src/components/string/utils.ts
+++ src/components/string/utils.ts
@@ -1,9 +1,46 @@
-import { ElementStates } from "../../types/element-states";
+import { SetStateAction } from 'react';
+import { DELAY_IN_MS } from '../../constants/delays';
+import { ElementStates } from '../../types/element-states';
+import { delay, swap } from '../../utils/utils';
 
 export const changeColor = (start: number, end: number, index: number) => {
-    if (start === index || end === index) {
-      return ElementStates.Changing;
-    } else if (start >= index || end <= index) {
-      return ElementStates.Modified;
-    } 
-  };
+  if (start === index || end === index) {
+    return ElementStates.Changing;
+  } else if (start >= index || end <= index) {
+    return ElementStates.Modified;
+  }
+};
+
+export const reverseString = async (
+  str: string,
+  setWordArr: (arg0: string[]) => void,
+  setIsLoader: (arg0: boolean) => void,
+  setIsDisabled: (arg0: boolean) => void,
+  setFirstIndex: (arg0: number) => void,
+  setSecondIndex: (arg0: number) => void
+) => {
+  const wordArray = str.split('');
+  setWordArr(wordArray);
+
+  let start = 0;
+  let end = wordArray.length - 1;
+  while (start <= end) {
+    setIsLoader(true);
+    setIsDisabled(true);
+    setFirstIndex(start);
+    setSecondIndex(end);
+    await delay(DELAY_IN_MS);
+    swap(wordArray, start, end);
+    setWordArr([...wordArray]);
+
+    await delay(DELAY_IN_MS);
+    start++;
+    end--;
+  }
+  setFirstIndex(100);
+  setSecondIndex(100);
+  setIsLoader(false);
+  setIsDisabled(false);
+
+  return wordArray.join('');
+};
